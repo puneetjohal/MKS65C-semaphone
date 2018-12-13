243404bd5b201bdf64a600d952e0a0519a7c6fdb
nadinejackson <njackson@stuy.edu>
2018-12-13 16:49:13 -0500

    trying to understand control code

--- setup.c
+++ setup.c
@@ -8,6 +8,14 @@
 #include <sys/shm.h>
 #include <unistd.h>
 
+#define KEY 0xDABDAB
+
+union semun {
+  int val; /* Value for SETVAL */
+  struct semid_ds *buf; /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short *array; /* Array for GETALL, SETALL */
+  struct seminfo *__buf; /* Buffer for IPC_INFO */
+};
 int * create()
 {
   printf("okay, I'm gonna check if resources are available\n");
@@ -16,9 +24,12 @@ int * create()
   key_t key = ftok("./telephone.game", 'R');
   int got = shmget(key, sizeof(int), 0644 | IPC_CREAT);
   int *ptr = shmat(got, 0, 0);
+  int semd = semget(KEY, 1, IPC_CREAT); //semd is the semaphore descriptor
+  semctl();
   return ptr;
 }
-void remove()
+
+void remove(int got)
 {
-  
+  shmctl(got, IPC_RMID, NULL);
 }
